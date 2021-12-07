52246051bccf57e0cd748f5d05af80cdd8b05c22
Lailanailussaadah <71623025+Lailanailussaadah@users.noreply.github.com>
2021-12-08 03:59:13 +0800

    Create Lingkaran.java

---
+++ ProjectBangun/src/bangundatar/Lingkaran.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bangundatar;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Lingkaran extends BangunDatar {
+    // atribut jari-jari
+    public double jari = 10;
+     
+    // method untuk hitung luas lingkaran
+    public double hitungLuas() {
+        double luas = 3.14 * this.jari * this.jari;
+        return luas;
+    }
+    
+    // method untuk hitung keliling
+    public double hitungKeliling() {
+        double keliling = 2 * 3.14 * this.jari;
+        return keliling;
+    }
+}
