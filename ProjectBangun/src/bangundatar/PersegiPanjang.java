50b13c11c5290a71de4d471801dc4985e55ff644
Lailanailussaadah <71623025+Lailanailussaadah@users.noreply.github.com>
2021-12-08 03:59:51 +0800

    Create PersegiPanjang.java

---
+++ ProjectBangun/src/bangundatar/PersegiPanjang.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bangundatar;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PersegiPanjang extends BangunDatar {
+    public double panjang = 5, lebar = 12;
+
+    public double hitungLuas() {
+        double luas = this.panjang * this.lebar;
+        return luas;
+    }
+
+    // method untuk hitung keliling
+    public double hitungKeliling() {
+        double keliling = 2 * (this.panjang + this.lebar);
+        return keliling;
+    }
+}
